4337ab872faaf9fd3c958fe9fb8b934b4208d630
shweta1462006 <shwetachauahan221101@gmail.com>
2025-06-20 14:01:35 +0530

    images_changes

--- blogPage/src/components/Online/Seactionnav.jsx
+++ blogPage/src/components/Online/Seactionnav.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import ClothsData from "./third45"
 
 function nav() {
   return (
     <div>
+      <script>
+        let result = ClothsData.match(/l\w+/gi);
+        console.log(result)
+
+
+      </script>
       <div className=' mt-10 h-20  w-100 relative flex items-center justify-around list-none hover:bg-gradient-to-r from-purple-400 via-pink-500 to-red-500   bg-gray-100'>
-        <li class="hover:bg-pink-500 hover:text-white px-4 py-2 cursor-pointer"><a>Women</a></li>
+        <li class="hover:bg-pink-500 hover:text-white px-4 py-2 cursor-pointer" ><a>Women</a></li>
         <li class="hover:bg-pink-500 hover:text-white px-4 py-2 cursor-pointer"><a>Men</a></li>
         <li class="hover:bg-pink-500 hover:text-white px-4 py-2 cursor-pointer"><a>Girl</a></li>
         <li class="hover:bg-pink-500 hover:text-white px-4 py-2 cursor-pointer"><a>Boy</a></li>
--- blogPage/src/components/Online/third45.jsx
+++ blogPage/src/components/Online/third45.jsx
@@ -4,228 +4,342 @@ const ClothsData ={
     {
         "id":1,
       "title": "     Women shop",
-      "url": "https://www.andindia.com/dw/image/v2/BGCX_PRD/on/demandware.static/-/Sites-masterCatalog_ANDIndia/default/dw62083bbf/images/hires/SS22/S25R118TP49_ORGWHT_1.jpg?sw=400&sh=600&sm=fit&strip=false",
+      "url": "https://image.hm.com/assets/hm/f8/f7/f8f7548462ad1ce7328403fada4d4abf40b91027.jpg?imwidth=1260",
       "dress_name": "Coral Wave Top",
       "price":165
     },
     {
           "id":2,
       "title": "     Women shop",
-      "url": "https://www.andindia.com/dw/image/v2/BGCX_PRD/on/demandware.static/-/Sites-masterCatalog_ANDIndia/default/dwbe16d731/images/hires/SS22/S25V110TDB_PINK_3.jpg?sw=400&sh=600&sm=fit&strip=false",
+      "url": "https://image.hm.com/assets/hm/ad/d9/add93be3ed23cc597443223e8242814d31e5ddc4.jpg?imwidth=1260",
       "dress_name": "Elgon Top",
       "price": 165
     },
     {
           "id":3,
       "title": "     Women shop",
-      "url": "https://www.andindia.com/dw/image/v2/BGCX_PRD/on/demandware.static/-/Sites-masterCatalog_ANDIndia/default/dwf1bee805/images/hires/SS22/S25P29DRPG_d_blue%20(6)%20copy.jpg?sw=400&sh=600&sm=fit&strip=false",
+      "url": "https://image.hm.com/assets/hm/01/74/01743a3d11b7cae4e809f8b31ed4094a5c432a0a.jpg?imwidth=1260",
       "dress_name": "Charcoal",
       "price": 89.99
     },
         {
           "id":4,
       "title": "     Women shop",
-      "url": "https://www.andindia.com/dw/image/v2/BGCX_PRD/on/demandware.static/-/Sites-masterCatalog_ANDIndia/default/dwdea2da57/images/hires/SS22/S25R1463P164_GRN-PRT_1.jpg?sw=400&sh=600&sm=fit&strip=false",
+      "url": "https://image.hm.com/assets/hm/ec/8f/ec8fb34a6727c7f219df1f6670d709650279d5d0.jpg?imwidth=1260",
       "dress_name": "Shadow Greens Co-ord",
       "price": 89.99
     },
     {
           "id":5,
       "title": "     Women shop",
-      "url": "https://www.andindia.com/dw/image/v2/BGCX_PRD/on/demandware.static/-/Sites-masterCatalog_ANDIndia/default/dw325efef9/images/hires/SS22/S25V298DRP84_BLUE_(1).jpg?sw=400&sh=600&sm=fit&strip=false",
+      "url": "https://image.hm.com/assets/hm/db/af/dbaf2dbcdad8a2e1c48c3be9fb6b3ed42b8b3ff7.jpg?imwidth=1260",
       "dress_name": "The Classic Striped Dress",
       "price": 89.99
     },
     {
           "id":6,
       "title": "     Women shop",
-      "url": "https://www.andindia.com/dw/image/v2/BGCX_PRD/on/demandware.static/-/Sites-masterCatalog_ANDIndia/default/dw276eaf76/images/hires/SS22/S25R119DRP450_GRN-PRT_5.jpg?sw=400&sh=600&sm=fit&strip=false",
+      "url": "https://image.hm.com/assets/hm/be/e8/bee8b4e52895e89941c184ccb1746a691ad09f14.jpg?imwidth=1260",
       "dress_name": "Alanis Dress",
       "price": 89.99
     },
     {
           "id":7,
       "title": "     Women shop",
-      "url": "https://www.andindia.com/dw/image/v2/BGCX_PRD/on/demandware.static/-/Sites-masterCatalog_ANDIndia/default/dw659dfdb2/images/hires/SS22/S25R129DRCAM_WHITE_(13).jpg?sw=400&sh=600&sm=fit&strip=false",
+      "url": "https://image.hm.com/assets/hm/3d/4f/3d4f94f4601aa9362209d5313f23c08c9690a9a4.jpg?imwidth=1260",
       "dress_name": "Alda Dress",
       "price": 89.99
     },
         {
           "id":8,
       "title": "     Women shop",
-      "url": "https://www.andindia.com/dw/image/v2/BGCX_PRD/on/demandware.static/-/Sites-masterCatalog_ANDIndia/default/dwa753dec4/images/hires/SS22/S25V275DRPYD_blue_prt%20(2)%20copy.jpg?sw=400&sh=600&sm=fit&strip=false",
+      "url": "https://image.hm.com/assets/hm/28/c2/28c2eda4b17078262c790c60c95c5739ddcd8fd9.jpg?imwidth=1260",
       "dress_name": "Light Blue Stripes Dress",
       "price": 89.99
     },
     {
           "id":9,
       "title": "     Women shop",
-      "url": "https://www.andindia.com/dw/image/v2/BGCX_PRD/on/demandware.static/-/Sites-masterCatalog_ANDIndia/default/dw5272e119/images/hires/SS22/S25V852PLV_NAVY%20(1)%20copy%202.jpg?sw=400&sh=600&sm=fit&strip=false",
+      "url": "https://image.hm.com/assets/hm/e1/88/e18879ce22621369e6a6c2558c7cf46176ccfac7.jpg?imwidth=1260",
       "dress_name": "The Navy Hue Co-ord",
       "price": 2669.99
     },    {
           "id":10,
       "title": "     Women shop",
-      "url": "https://www.andindia.com/dw/image/v2/BGCX_PRD/on/demandware.static/-/Sites-masterCatalog_ANDIndia/default/dw89fc4561/images/hires/SS22/S25V40DRP75-WHT-BLK%20(4)%20copy.jpg?sw=400&sh=600&sm=fit&strip=false",
+      "url": "https://image.hm.com/assets/hm/ca/58/ca584338514119e71a24bfba267ef4fa07d86c1e.jpg?imwidth=1260",
       "dress_name": "Stripe Every Way Wrap Dress",
       "price": 1664.99
     },    {
           "id":11,
       "title": "     Women shop",
-      "url": "https://www.andindia.com/dw/image/v2/BGCX_PRD/on/demandware.static/-/Sites-masterCatalog_ANDIndia/default/dw80e21525/images/hires/SS22/S25V157TW58_BEIGE_(5)%20copy.jpg?sw=400&sh=600&sm=fit&strip=false",
+      "url": "https://image.hm.com/assets/hm/de/89/de89402687176492c4b3dc3c78c2f6194099f89c.jpg?imwidth=1260",
       "dress_name": "Abstract Mood To",
       "price": 89.99
     },    {
           "id":12,
       "title": "     Women shop",
-      "url": "https://www.andindia.com/dw/image/v2/BGCX_PRD/on/demandware.static/-/Sites-masterCatalog_ANDIndia/default/dw659dfdb2/images/hires/SS22/S25R129DRCAM_WHITE_(13).jpg?sw=400&sh=600&sm=fit&strip=false",
+      "url": "https://image.hm.com/assets/hm/83/71/8371882b8676780cc674aceb903b32d1162137a7.jpg?imwidth=1260",
       "dress_name": "Alda Dress",
       "price": 89.99
     },
+      {
+        "id":13,
+      "title": "     Women shop",
+            "url": "https://image.hm.com/assets/hm/bf/bc/bfbc2fc6aa3b3d0d14038e87f01a1a79ce9e246f.jpg?imwidth=1260",
+
+      "dress_name": "Coral Wave Top",
+      "price":165
+    },
+      {
+        "id":14,
+      "title": "     Women shop",
+      "url": "https://image.hm.com/assets/hm/27/58/27585f894cbba380274175ba334d25215539107c.jpg?imwidth=1260",
+      "dress_name": "Coral Wave Top",
+      "price":165
+    }, 
+     {
+        "id":15,
+      "title": "     Women shop",
+      "url": "https://image.hm.com/assets/hm/9b/d4/9bd4ced79df3cd0d7f1b27034ba497b28118dff0.jpg?imwidth=1260",
+      "dress_name": "Coral Wave Top",
+      "price":165
+    },  {
+        "id":16,
+      "title": "     Women shop",
+      "url": "https://image.hm.com/assets/hm/95/0c/950c1a82f861bdb061b23fee84b676a382547f46.jpg?imwidth=1260",
+      "dress_name": "Coral Wave Top",
+      "price":165
+    }
 
 
   ],
    men:[
         {
-        "id":13,
-      "title": "     Women shop",
-      "url": "https://image.hm.com/assets/hm/47/8e/478eb6823ce709ea8aff6dc8a7adaf621eff38bf.jpg?imwidth=1260",
-      "dress_name": "Coral Wave Top",
-      "price":165
-    },
-    {
-        "id":14,
-      "title": "     Women shop",
-      "url": "https://image.hm.com/assets/hm/47/8e/478eb6823ce709ea8aff6dc8a7adaf621eff38bf.jpg?imwidth=1260",
-      "dress_name": "Coral Wave Top",
-      "price":165
-    },
-    {
-        "id":15,
-      "title": "     Women shop",
-      "url": "https://image.hm.com/assets/hm/47/8e/478eb6823ce709ea8aff6dc8a7adaf621eff38bf.jpg?imwidth=1260",
-      "dress_name": "Coral Wave Top",
-      "price":165
-    },
-    {
-        "id":16,
-      "title": "     Women shop",
-      "url": "https://image.hm.com/assets/hm/47/8e/478eb6823ce709ea8aff6dc8a7adaf621eff38bf.jpg?imwidth=1260",
-      "dress_name": "Coral Wave Top",
-      "price":165
-    },
-    {
         "id":17,
       "title": "     Women shop",
-      "url": "https://image.hm.com/assets/hm/47/8e/478eb6823ce709ea8aff6dc8a7adaf621eff38bf.jpg?imwidth=1260",
+      "url": "https://image.hm.com/assets/hm/2f/37/2f3728dd2edbd5b2fdc030561fc32ff94fde4785.jpg?imwidth=1260",
       "dress_name": "Coral Wave Top",
       "price":165
     },
-      {
+    {
         "id":18,
       "title": "     Women shop",
-      "url": "https://image.hm.com/assets/hm/47/8e/478eb6823ce709ea8aff6dc8a7adaf621eff38bf.jpg?imwidth=1260",
-      "dress_name": "Coral Wave Top",
-      "price":165
-    },  {
-        "id":19,
-      "title": "     Women shop",
-      "url": "https://image.hm.com/assets/hm/47/8e/478eb6823ce709ea8aff6dc8a7adaf621eff38bf.jpg?imwidth=1260",
-      "dress_name": "Coral Wave Top",
-      "price":165
-    },  {
-        "id":20,
-      "title": "     Women shop",
-      "url": "https://image.hm.com/assets/hm/47/8e/478eb6823ce709ea8aff6dc8a7adaf621eff38bf.jpg?imwidth=1260",
+      "url": "https://image.hm.com/assets/hm/fe/47/fe4713859ebdf4928718ffb1d68dca4aac57aea8.jpg?imwidth=1260",
       "dress_name": "Coral Wave Top",
       "price":165
     },
-        {
+    {
+        "id":19,
+      "title": "     Women shop",
+      "url": "https://image.hm.com/assets/hm/3d/f6/3df6c75832b9dd151f92b498f1d061cc1c204566.jpg?imwidth=1260",
+      "dress_name": "Coral Wave Top",
+      "price":165
+    },
+    {
+        "id":20,
+      "title": "     Women shop",
+      "url": "https://image.hm.com/assets/hm/7d/af/7dafe89d9136400a9418bc01a3a4e0b83306a432.jpg?imwidth=1260",
+      "dress_name": "Coral Wave Top",
+      "price":165
+    },
+    {
         "id":21,
       "title": "     Women shop",
-      "url": "https://image.hm.com/assets/hm/47/8e/478eb6823ce709ea8aff6dc8a7adaf621eff38bf.jpg?imwidth=1260",
+      "url": "https://image.hm.com/assets/hm/e0/21/e0216ee5b942b63aa1b95b5f3604f9473eda5daf.jpg?imwidth=1260",
       "dress_name": "Coral Wave Top",
       "price":165
     },
       {
         "id":22,
       "title": "     Women shop",
-      "url": "https://image.hm.com/assets/hm/47/8e/478eb6823ce709ea8aff6dc8a7adaf621eff38bf.jpg?imwidth=1260",
+      "url": "https://image.hm.com/assets/hm/9e/b1/9eb1f106fef31d0f38274ee58c874835d2f87d5c.jpg?imwidth=1260",
       "dress_name": "Coral Wave Top",
       "price":165
     },  {
         "id":23,
       "title": "     Women shop",
-      "url": "https://image.hm.com/assets/hm/47/8e/478eb6823ce709ea8aff6dc8a7adaf621eff38bf.jpg?imwidth=1260",
+      "url": "https://image.hm.com/assets/hm/6e/7e/6e7ece6baad7bbf317a47a54d2f0e81331c3a280.jpg?imwidth=1260",
       "dress_name": "Coral Wave Top",
       "price":165
     },  {
         "id":24,
       "title": "     Women shop",
-      "url": "https://image.hm.com/assets/hm/47/8e/478eb6823ce709ea8aff6dc8a7adaf621eff38bf.jpg?imwidth=1260",
+      "url": "https://image.hm.com/assets/hm/ff/ed/ffed3f0b578acbf9bd178dc3bf003822799c038b.jpg?imwidth=1260",
       "dress_name": "Coral Wave Top",
       "price":165
-    }
-  ],
-  kids:[
+    },
         {
         "id":25,
       "title": "     Women shop",
-      "url": "https://image.hm.com/assets/hm/7c/63/7c6347d5303964c68695d468c3c34030473710c2.jpg?imwidth=1536",
+      "url": "https://image.hm.com/assets/hm/1d/6b/1d6bb7e68853f9775d323c29d9c7871f5b268d77.jpg?imwidth=1260",
       "dress_name": "Coral Wave Top",
       "price":165
     },
       {
         "id":26,
       "title": "     Women shop",
-      "url": "https://image.hm.com/assets/hm/21/1b/211b625296d26e99746a5e00e16ee6eace088d30.jpg?imwidth=1536",
+      "url": "https://image.hm.com/assets/hm/9f/01/9f0101b7589ea3fa7219ea37b1b5950b4b3d5975.jpg?imwidth=1260",
+      "dress_name": "Coral Wave Top",
+      "price":165
+    }, 
+     {
+        "id":27,
+      "title": "     Women shop",
+      "url": "https://image.hm.com/assets/hm/e8/df/e8dffe6873124f3d8fb986e8f5af904a2c0f0789.jpg?imwidth=1260",
       "dress_name": "Coral Wave Top",
       "price":165
     },  {
-        "id":27,
+        "id":28,
       "title": "     Women shop",
-      "url": "https://image.hm.com/assets/hm/40/e2/40e2dccc3bcdb960de0b3ccadd8e65dd715afc00.jpg?imwidth=1536",
+      "url": "https://image.hm.com/assets/hm/2b/49/2b4923bf14054c2fba3da31bbf64512c7cef56d3.jpg?imwidth=1260",
       "dress_name": "Coral Wave Top",
       "price":165
     },
       {
-        "id":28,
+        "id":29,
       "title": "     Women shop",
-      "url": "https://image.hm.com/assets/hm/12/ae/12aeb399ae000696c0d6222b50303c803f585e33.jpg?imwidth=1536",
+      "url": "https://image.hm.com/assets/hm/6a/5a/6a5ab82f843eab1514f54b89f7b31ffc7bd3faf5.jpg?imwidth=1260",
+      "dress_name": "Coral Wave Top",
+      "price":165
+    },
+      {
+        "id":30,
+      "title": "     Women shop",
+      "url": "https://image.hm.com/assets/hm/ad/fd/adfdea61f0a602a7f0877b1e63d2734af3c657c6.jpg?imwidth=1260",
+      "dress_name": "Coral Wave Top",
+      "price":165
+    }, 
+     {
+        "id":31,
+      "title": "     Women shop",
+      "url": "https://image.hm.com/assets/hm/fa/84/fa84b45a9c3b2aefc36304dfa36841a2d220641a.jpg?imwidth=1260",
+      "dress_name": "Coral Wave Top",
+      "price":165
+    },  {
+        "id":32,
+      "title": "     Women shop",
+      "url": "https://image.hm.com/assets/hm/67/ef/67efc9724da7bcbb7714895565b096ccf116e215.jpg?imwidth=1260",
+      "dress_name": "Coral Wave Top",
+      "price":165
+    }
+  ],
+  kids:[
+        {
+        "id":33,
+      "title": "     Women shop",
+      "url": "https://image.hm.com/assets/hm/48/02/480290f558f21c010f2056c788fa5ff5944bdfff.jpg?imwidth=1260",
+      "dress_name": "Coral Wave Top",
+      "price":165
+    },
+      {
+        "id":34,
+      "title": "     Women shop",
+      "url": "https://image.hm.com/assets/hm/c4/da/c4dad365a06fdc29692403bbf5379dfc6b8bd9a0.jpg?imwidth=1260",
+      "dress_name": "Coral Wave Top",
+      "price":165
+    },  {
+        "id":35,
+      "title": "     Women shop",
+      "url": "https://image.hm.com/assets/hm/8d/98/8d983190aa3ddb89a587f1b4ef2d7d5d160a108d.jpg?imwidth=1260",
+      "dress_name": "Coral Wave Top",
+      "price":165
+    },
+      {
+        "id":36,
+      "title": "     Women shop",
+      "url": "https://image.hm.com/assets/hm/d2/22/d222c0fb91cf57662cbd2f86a9e6d6e85cd5c198.jpg?imwidth=1260",
       "dress_name": "Coral Wave Top",
       "price":165
     },
           {
-        "id":29,
+        "id":37,
       "title": "     Women shop",
-      "url": "https://image.hm.com/assets/hm/40/e2/40e2dccc3bcdb960de0b3ccadd8e65dd715afc00.jpg?imwidth=1536",
+      "url": "https://image.hm.com/assets/hm/f9/b3/f9b36fd4312726a80b6b86b34ec991431bd2fa42.jpg?imwidth=1260",
       "dress_name": "Coral Wave Top",
       "price":165
     },
 
       {
-        "id":30,
+        "id":38,
       "title": "     Women shop",
-      "url": "https://image.hm.com/assets/hm/40/e2/40e2dccc3bcdb960de0b3ccadd8e65dd715afc00.jpg?imwidth=1536",
+      "url": "https://image.hm.com/assets/hm/9e/c7/9ec7545780c0d31e848058bd73500d65616fc052.jpg?imwidth=1260",
       "dress_name": "Coral Wave Top",
       "price":165
     },
 
       {
-        "id":31,
+        "id":39,
       "title": "     Women shop",
-      "url": "https://image.hm.com/assets/hm/40/e2/40e2dccc3bcdb960de0b3ccadd8e65dd715afc00.jpg?imwidth=1536",
+      "url": "https://image.hm.com/assets/hm/71/93/71933f98061e99f201935b283bbf9ec5448e2a63.jpg?imwidth=1260",
       "dress_name": "Coral Wave Top",
       "price":165
     },
 
       {
-        "id":32,
+        "id":40,
       "title": "     Women shop",
-      "url": "https://image.hm.com/assets/hm/40/e2/40e2dccc3bcdb960de0b3ccadd8e65dd715afc00.jpg?imwidth=1536",
+      "url": "https://image.hm.com/assets/hm/d6/b7/d6b77bf93ba90b8090355e6fefc4c360bc17e29c.jpg?imwidth=1260",
       "dress_name": "Coral Wave Top",
       "price":165
     },
+      {
+        "id":41,
+      "title": "     Women shop",
+      "url": "https://image.hm.com/assets/hm/63/e9/63e931359e6548216d4dcef6bb8cffcc12349982.jpg?imwidth=1260",
+      "dress_name": "Coral Wave Top",
+      "price":165
+    },  {
+        "id":42,
+      "title": "     Women shop",
+      "url": "https://image.hm.com/assets/hm/e2/12/e2123c00fe26ac38a2d0eec2237fca1ceb7dc4d3.jpg?imwidth=1260",
+      "dress_name": "Coral Wave Top",
+      "price":165
+    },  {
+        "id":43,
+      "title": "     Women shop",
+      "url": "https://image.hm.com/assets/hm/fd/01/fd0133ededafded141b2a157196c41c38ec79931.jpg?imwidth=1260",
+      "dress_name": "Coral Wave Top",
+      "price":165
+    },  {
+        "id":44,
+      "title": "     Women shop",
+      "url": "https://image.hm.com/assets/hm/47/bb/47bbbfa85fb88eae34a22553fb3cbc6f444bcdf7.jpg?imwidth=1260",
+      "dress_name": "Coral Wave Top",
+      "price":165
+    },
+
+ {
+        "id":45,
+      "title": "     Women shop",
+      "url": "https://image.hm.com/assets/hm/9c/bd/9cbd106953e29d7c272eb45ac5ad974724783b2a.jpg?imwidth=1260",
+      "dress_name": "Coral Wave Top",
+      "price":165
+    },
+     {
+        "id":46,
+      "title": "     Women shop",
+      "url": "https://image.hm.com/assets/hm/8f/b8/8fb88431d7df2bc1270ffafc5ec40cff9d7d74b4.jpg?imwidth=1260",
+      "dress_name": "Coral Wave Top",
+      "price":165
+    },
+
+ {
+        "id":47,
+      "title": "     Women shop",
+      "url": "https://image.hm.com/assets/hm/09/ff/09ffcb2523196c01d98c3437e9e53acd8b694f1c.jpg?imwidth=1260",
+      "dress_name": "Coral Wave Top",
+      "price":165
+    },
+
+ {
+        "id":48,
+      "title": "     Women shop",
+      "url": "https://image.hm.com/assets/hm/38/c2/38c26d541a1029e0e7c9ffea4b38434b23d5fe2d.jpg?imwidth=1260  ",
+      "dress_name": "Coral Wave Top",
+      "price":165
+    },
+
+
 
 
 
